c4efd9927b3db56dc22cfb74431b7c32cd89ab26
Olga Andrievich <olgakuz2302@gmail.com>
2023-06-21 11:53:50 +0300

    added header reactions to user login changes

--- meetups/src/app/modules/header/header.component.ts
+++ meetups/src/app/modules/header/header.component.ts
@@ -1,11 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  isAuth: boolean = false;
+export class HeaderComponent implements OnInit {
+  isAuth?: boolean;
+  
+  constructor(private authService: AuthService) { }
+  
+  ngOnInit() {
+    this.authService.currentUserToken!.subscribe((data) => (this.isAuth = !!data));
+  }
+  
+  logout() {
+    this.authService.logout();
+    this.isAuth = false;
+  }
 }
