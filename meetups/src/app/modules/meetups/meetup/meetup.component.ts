4c1d4138fcb8667cf9c6d848550d25e03e7fa0a5
Olga Andrievich <olgakuz2302@gmail.com>
2023-06-28 15:30:21 +0300

    fixed meetup subscribers and time format

--- meetups/src/app/modules/meetups/meetup/meetup.component.ts
+++ meetups/src/app/modules/meetups/meetup/meetup.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Meetup } from 'src/app/classes/meetup';
+import { checkWordDeclension } from 'src/app/helpers/utils';
 
 @Component({
   selector: 'app-meetup',
@@ -8,7 +16,6 @@ import { Meetup } from 'src/app/classes/meetup';
   styleUrls: ['./meetup.component.scss'],
 })
 export class MeetupComponent implements OnInit {
-
   @Input() public meetup!: Meetup;
   @Input() public userId!: number;
   @Output() public signMeetupEvent = new EventEmitter();
@@ -16,16 +23,18 @@ export class MeetupComponent implements OnInit {
 
   longDescription: boolean = false;
   public canComeBtnVisible!: boolean;
-  
+
   ngOnInit() {
     this.checkUserSign();
+    // console.log('new date', new Date())
   }
 
   changeDescription() {
     this.longDescription = !this.longDescription;
-    console.log(this.meetup)
+    console.log(this.meetup);
+    this.parseTime(this.meetup.time);
   }
-  
+
   signMeetup() {
     this.checkUserSign();
     if (this.canComeBtnVisible) {
@@ -33,7 +42,7 @@ export class MeetupComponent implements OnInit {
     }
     this.canComeBtnVisible = false;
   }
-  
+
   signOverMeetup() {
     this.checkUserSign();
     if (!this.canComeBtnVisible) {
@@ -41,8 +50,43 @@ export class MeetupComponent implements OnInit {
     }
     this.canComeBtnVisible = true;
   }
-  
+
   checkUserSign() {
-    this.canComeBtnVisible = !!!this.meetup.users.find(user => user.id === this.userId)
+    this.canComeBtnVisible = !!!this.meetup.users.find(
+      (user) => user.id === this.userId
+    );
+  }
+
+  checkDeclension(number: number, words: Array<string>) {
+    return checkWordDeclension(number, words);
+  }
+
+  parseTime(time: string) {
+    const date = new Date(time);
+    const day =
+      date.getDate().toString().length < 2
+        ? '0' + date.getDate()
+        : date.getDate();
+    const month =
+      date.getMonth().toString().length < 2
+        ? '0' + (date.getMonth() + 1)
+        : date.getMonth() + 1;
+    const year = date.getFullYear().toString().slice(-2);
+
+    const hours =
+      date.getHours().toString().length < 2
+        ? '0' + date.getHours()
+        : date.getHours();
+    const minutes =
+      date.getMinutes().toString().length < 2
+        ? '0' + date.getMinutes()
+        : date.getMinutes();
+    const seconds =
+      date.getSeconds().toString().length < 2
+        ? '0' + date.getSeconds()
+        : date.getSeconds();
+
+    const meetupDate = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+    return meetupDate;
   }
 }
