492003e5784caeb4b49554fb7aa8a89c21126633
Olga Andrievich <olgakuz2302@gmail.com>
2023-06-28 23:51:49 +0300

    added link to meetup creation

--- meetups/src/app/modules/meetups/my-meetups/my-meetups.module.ts
+++ meetups/src/app/modules/meetups/my-meetups/my-meetups.module.ts
@@ -4,10 +4,11 @@ import { MyMeetupsComponent } from './my-meetups.component';
 import { MeetupModule } from '../meetup/meetup.module';
 import { SearchBarModule } from '../search-bar/search-bar.module';
 import { PaginationModule } from '../pagination/pagination.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [MyMeetupsComponent],
-  imports: [CommonModule, MeetupModule, SearchBarModule, PaginationModule],
+  imports: [CommonModule, MeetupModule, SearchBarModule, PaginationModule, AppRoutingModule],
   exports: [MyMeetupsComponent],
 })
 export class MyMeetupsModule {}
