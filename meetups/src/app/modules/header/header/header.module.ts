b5b0c5825a81bea98ee6a886afb3a4b482afdeaa
Olga Andrievich <olgakuz2302@gmail.com>
2023-06-30 11:25:52 +0300

    added adaptive

--- meetups/src/app/modules/header/header/header.module.ts
+++ meetups/src/app/modules/header/header/header.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { NavbarModule } from '../navbar/navbar.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, NavbarModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    NavbarModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
 })
 export class HeaderModule {}
--- meetups/src/app/modules/header/navbar/navbar.module.ts
+++ meetups/src/app/modules/header/navbar/navbar.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
   ],
-  exports: [
-    NavbarComponent
-  ]
+  exports: [NavbarComponent],
 })
-export class NavbarModule { }
+export class NavbarModule {}
